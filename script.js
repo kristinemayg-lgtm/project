a5a44afa96751f37a61969b4abb845cd35230565
kristinemayg-lgtm <kristinemayg@gmail.com>
2025-11-04 15:34:08 -0500

    contact

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("messageError").textContent = "";
+
+    let isValid = true;
+
+    const name = document.getElementById("name").value.trim();
+    if (name === "") {
+        document.getElementById("nameError").textContent = "Name is required.";
+        isValid = false;
+    }
+    const email = document.getElementById("email").value.trim();
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailPattern.test(email)) {
+        document.getElementById("emailError").textContent = "Please enter a valid email.";
+        isValid = false;
+    }  
+    const message = document.getElementById("message").value.trim();
+    if (message.length < 10) {
+        document.getElementById("messageError").textContent = "Message must be at least 10 characters.";
+        isValid = false;
+    }
+    if (isValid) {
+        alert("Form submitted successfully!");
+        form.reset();
+    }
+  });
